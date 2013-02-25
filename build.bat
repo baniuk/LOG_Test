43b3659baebcc3a8c1379e3d543a650fcb3bb10d
baniuk <baniuk@zut.edu.pl>
2013-02-25 22:00:47 +0100

    kompatybilnosc z VC10

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rem temporary script for testing cmake
 rem option ALL cleans everything
 call tools\clean.bat %1
-cmake -G "NMake Makefiles"
+cmake -G "NMake Makefiles" --graphviz=CMake_trash\ARCH.dot
 rem calls warper script - correct env must be set http://www.mail-archive.com/cmake@cmake.org/msg38597.html
 CMake_trash\make_wrapper_script.bat
