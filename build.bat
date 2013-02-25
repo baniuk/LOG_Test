45a4052ba3db3c460ccb8a68e27982c32bea730d
baniuk <baniuk@zut.edu.pl>
2013-02-25 15:44:25 +0100

    add out-of-source build

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 rem temporary script for testing cmake
 rem option ALL cleans everything
 call tools\clean.bat %1
-cmake -G "NMake Makefiles"
+rem out-of-source build
+mkdir bin
+cd bin
+cmake -G "NMake Makefiles" ..\
 rem calls warper script - correct env must be set http://www.mail-archive.com/cmake@cmake.org/msg38597.html
-CMake_trash\make_wrapper_script.bat
+..\CMake_trash\make_wrapper_script.bat
+cd ..
