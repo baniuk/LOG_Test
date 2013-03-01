eedd8c7adc3ec821090a57b7376ba080c6f9bc0e
baniuk <baniuk@zut.edu.pl>
2013-03-01 15:22:12 +0100

    poprawiony bład w clean.bat oraz dorobiona dokumntacja w pliku

--- tools/clean.bat
+++ tools/clean.bat
@@ -1,3 +1,15 @@
+rem Czyœci wszystkie wystapienia:
+rem 1. CMakeCache
+rem 2. cmake_install
+rem 3. CMakeFiles
+rem 4. Katalog bin
+
+rem Dodatkowo, jeœli parametr wejsciowy jest ALL to czyœci:
+rem 5. CMake_trash (ponowny download)
+rem 6. doc/html
+rem 7. Makefile
+rem 8. Zawartoœæ Third_Party (ponowna kompilacja)
+
 echo off
 SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 goto errorext
@@ -5,8 +17,8 @@ IF ERRORLEVEL 1 goto errorext
 FOR /R %%A IN (*) DO IF %%~nA%%~xA==cmake_install.cmake del /q %%~fA
 FOR /R %%A IN (*) DO IF %%~nA%%~xA==CMakeCache.txt del /q %%~fA
 FOR /D /R %%X IN (CMakeFiles*) DO rmdir "%%X" /s /q
-IF "%1"=="ALL" goto :CLEAR_ALL
 rmdir bin /s /q
+IF "%1"=="ALL" goto :CLEAR_ALL
 goto :EOF
 
 :CLEAR_ALL
