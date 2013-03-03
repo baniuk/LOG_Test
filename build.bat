2978fb01828ae8a5ed5351a5a7e03fdc059b0f52
baniuk <baniuk@zut.edu.pl>
2013-03-03 19:42:27 +0100

    komentarze do kodu

--- build.bat
+++ build.bat
@@ -6,5 +6,6 @@ mkdir bin
 cd bin
 cmake -G "NMake Makefiles" --graphviz=..\CMake_trash\ARCH.dot ..\
 rem calls warper script - correct env must be set http://www.mail-archive.com/cmake@cmake.org/msg38597.html
-..\CMake_trash\make_wrapper_script.bat
+call ..\CMake_trash\make_wrapper_script.bat
+nmake
 cd ..
