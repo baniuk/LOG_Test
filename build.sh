3568a77aa34e4ff9b25f66f413711c99122f845a
= <=>
2013-03-21 21:26:29 +0100

    fixed errors MA0002 MA0003

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ tools/clean.sh $1
 mkdir bin
 cd bin
 cmake -G "Unix Makefiles" --graphviz=../CMake_trash/ARCH.dot ../
-../CMake_trash/make_wrapper_script.bat
+. ../CMake_trash/make_wrapper_script.sh
 make
 cd ..
 
